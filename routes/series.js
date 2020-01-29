5f9b8d272e53ff37275d35fdab307e0f55f68ad5
mandimartins <amandamartins23@gmail.com>
2020-01-29 10:23:04 -0300

    Add validator middleware to series route

--- routes/series.js
+++ routes/series.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const Serie = require('../models/serie')
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'xyz1234abcd4321'
+
+
+router.use(async(req, res, next)=>{
+    const token = req.headers['x-access-token'] || req.body.token || req.query.token
+
+    if(token){
+
+        try{
+            const payload = jwt.verify(token, jwtSecret)
+            console.log(payload)
+            if(payload.roles.includes('restrito')){
+                next()
+            }else{
+                res.send({success:false})
+            }
+            
+        }catch(e){
+            res.send({success:false})
+        }
+        
+    }else{
+        res.send({success:false})
+    }
+})
+
 router.get('/',async(req,res)=>{
     const series = await Serie.find({})
     res.send(series)
