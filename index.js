5aba97cf233926add957d395dbd5a5b713f1d8db
mandimartins <amandamartins23@gmail.com>
2020-01-29 09:16:29 -0300

    Send token to the user

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongo = process.env.MONGO || 'mongodb://localhost/minhas-series-rest'
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 const User = require('./models/user')
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'xyz1234abcd4321'
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -32,6 +34,31 @@ const createInitialUsers = async () => {
 }
 
 app.use('/series',series)
+
+app.post('/auth',async(req, res) =>{
+    const user = req.body
+    const userDb =  await User.findOne({username: user.username})
+    if(userDb){
+        if(userDb.password === user.password){
+            const payload = {
+                id:userDb._id,
+                username:userDb.username,
+                roles: userDb.roles
+            }
+            jwt.sign(payload, jwtSecret,(err, token)=>{
+                res.send({
+                    success:true,
+                    token: token
+                })
+            })
+
+        }else{
+            res.send({success: false, message:'Wrong credentials'})
+        }
+    }else{
+        res.send({success: false, message:'Wrong credentials'})
+    }
+})
 mongoose
     .connect(mongo,{useNewUrlParser:true})
     .then(()=>{
