96d9074e63c1f9ff9bd4944bb2ab8785f1046d21
mandimartins <mandimartins23@gmail.com>
2020-03-01 19:06:12 -0300

    Encrypt password

--- models/user.js
+++ models/user.js
@@ -1,9 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    roles: [String]
-})
+  username: String,
+  password: String,
+  roles: [String]
+});
 
-const User = mongoose.model('User', UserSchema)
-module.exports = User
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt((err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,62 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-const jwtSecret = 'xyz1234abcd4321'
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const jwtSecret = 'xyz1234abcd4321';
 
+router.use(async (req, res, next) => {
+  const token =
+    req.headers['x-access-token'] || req.body.token || req.query.token;
 
-router.use(async(req, res, next)=>{
-    const token = req.headers['x-access-token'] || req.body.token || req.query.token
-
-    if(token){
-        try{
-            const payload = jwt.verify(token, jwtSecret)
-            if(payload.roles.includes('admin')){
-                console.log(payload)
-                next()
-            }else{
-                res.send({success:false})
-            }
-            
-        }catch(e){
-            res.send({success:false})
-        }
-        
-    }else{
-        res.send({success:false})
+  if (token) {
+    try {
+      const payload = jwt.verify(token, jwtSecret);
+      if (payload.roles.includes('admin')) {
+        next();
+      } else {
+        res.json({ success: false });
+      }
+    } catch (e) {
+      res.json({ success: false });
     }
-})
+  } else {
+    res.json({ success: false });
+  }
+});
 
-router.get('/', async(req, res)=>{
-    const users = await User.find({})
-    res.send(users)
-})
+router.get('/', async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+router.post('/new', async (req, res) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+      roles: req.body.roles
+    });
 
-module.exports = router
+    await user.save();
+
+    res.json({ success: true });
+  } catch (error) {
+    res.json({
+      success: false,
+      errors: Object.keys(e.errors)
+    });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.json({ success: true });
+  } catch (error) {
+    res.json({
+      success: false,
+      errors: Object.keys(e.errors)
+    });
+  }
+});
+
+module.exports = router;
